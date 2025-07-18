ddf1155b9ab4afb38ace940fd3141a5229355b4b
RanaUniverse <RanaUniverse321@gmail.com>
2025-07-18 08:49:12 +0530

    screensaver installation has been added, the bucklespring build from source also added in the installation script

--- installation_scripts_for_xfce4_desktop.sh
+++ installation_scripts_for_xfce4_desktop.sh
@@ -2,61 +2,74 @@
 
 
 echo "🔧 XFCE4 Desktop Environment will install on this machine..."
-sudo dpkg -i 1_dependencies_xfce4/*.deb || sudo apt-get -f install -y
+sudo dpkg -i 1_dependencies_xfce4/*.deb 
 echo "✅ XFCE4 has been installed."
 
 
 echo "🔧 Installing WHISKERMENU plugin..."
-sudo dpkg -i 2_dependencies_whiskermenu/*.deb || sudo apt-get -f install -y
+sudo dpkg -i 2_dependencies_whiskermenu/*.deb 
 echo "✅ WHISKERMENU plugin has been installed."
 
 
 echo "🔧 Installing CLIPMAN plugin..."
-sudo dpkg -i 3_dependencies_clipman/*.deb || sudo apt-get -f install -y
+sudo dpkg -i 3_dependencies_clipman/*.deb 
 echo "✅ CLIPMAN plugin has been installed."
 
 
 echo "🔧 Installing POWER MANAGER..."
-sudo dpkg -i 4_dependencies_power_manager/*.deb || sudo apt-get -f install -y
+sudo dpkg -i 4_dependencies_power_manager/*.deb 
 echo "✅ POWER MANAGER has been installed."
 
 
 echo "🔧 Installing SYSTEM LOAD plugin..."
-sudo dpkg -i 5_dependencies_systemload/*.deb || sudo apt-get -f install -y
+sudo dpkg -i 5_dependencies_systemload/*.deb 
 echo "✅ SYSTEM LOAD plugin has been installed."
 
 
 echo "🔧 Installing EYES plugin..."
-sudo dpkg -i 6_dependencies_eyes_plugin/*.deb || sudo apt-get -f install -y
+sudo dpkg -i 6_dependencies_eyes_plugin/*.deb 
 echo "✅ EYES plugin has been installed."
 
 
 echo "🔧 Installing SCREENSHOT tool..."
-sudo dpkg -i 7_dependencies_screenshooter/*.deb || sudo apt-get -f install -y
+sudo dpkg -i 7_dependencies_screenshooter/*.deb 
 echo "✅ SCREENSHOT tool has been installed."
 
 
 echo "🔧 Installing REDSHIFT dependencies..."
-sudo dpkg -i 8_dependencies_redshift/*.deb || sudo apt-get -f install -y
+sudo dpkg -i 8_dependencies_redshift/*.deb 
 echo "✅ REDSHIFT has been installed."
 
 
 echo "🔧 Installing BUCKLESPRING keyboard sound..."
-sudo dpkg -i 9_dependencies_bucklespring/*.deb || sudo apt-get -f install -y
+sudo dpkg -i 9_dependencies_bucklespring/*.deb 
 echo "✅ BUCKLESPRING has been installed."
+echo "The bucklespring setup build need to be done here manually for now.🍌🍌🍌"
+
+
+
+echo "🔧 Installing SCREENSAVER..."
+sudo dpkg -i 10_dependencies_screensaver/*.deb 
+echo "✅ SCREENSAVER has been installed."
+
+
+
+
 
 
 echo "🎉 All components have been installed successfully!"
 
 
-./files_and_folders/terminal_alias_for_redshift.sh
-
-./files_and_folders/terminal_alias_for_bucklespring.sh
-
-
-
 mkdir -pv ~/Pictures/screenshots
 cp -rv files_and_folders/scripts_for_shortcuts/ ~/.apps_and_softwares/
+
+
+
+./files_and_folders/terminal_alias_for_redshift.sh
+./files_and_folders/terminal_alias_for_bucklespring.sh
+
+./files_and_folders/build_bucklespring_from_source.sh 
+
 
 
 echo "Now i will need to setup the `~/.config/xfce4` files to make my settings of my desktop interface and some others."
